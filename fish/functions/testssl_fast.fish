daa3f4e246845ca7d5f147ee7977eb34ff8fda55
Stefan Crain <stefancrain@gmail.com>
2021-03-06 11:44:23 -0500

    fix: finish rename of testssl_fast

--- fish/functions/testssl_fast.fish
+++ fish/functions/testssl_fast.fish
@@ -1,3 +1,3 @@
-function ssl_fast --description 'quickly test ssl for a domain'
+function testssl_fast --description 'quickly test ssl for a domain'
     docker run --rm -ti drwetter/testssl.sh --quiet --sneaky --server-defaults $argv
 end
