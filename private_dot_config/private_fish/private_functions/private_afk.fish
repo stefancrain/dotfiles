5a793171d07d4c32150f6f51ea096c9fc94c4a30
Stefan Crain <stefan.crain@gmail.com>
2021-04-04 09:10:41 -0400

    better afk, with template

--- private_dot_config/private_fish/private_functions/private_afk.fish
+++
@@ -1,8 +0,0 @@
-function afk --description 'Lock the box'
-    switch (uname)
-        case Linux
-            command sudo systemctl suspend
-        case Darwin
-            command /System/Library/CoreServices/Menu\ Extras/User.menu/Contents/Resources/CGSession -suspend
-    end
-end
