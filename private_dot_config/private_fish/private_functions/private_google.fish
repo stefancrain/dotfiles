6ee6dd6c0db4cae33bf298e66df084cf2aa3f5ff
Stefan Crain <stefan.crain@gmail.com>
2021-04-05 09:44:45 -0400

    Google search as template

--- private_dot_config/private_fish/private_functions/private_google.fish
+++
@@ -1,8 +0,0 @@
-function google --description 'Google Search'
-    switch (uname)
-        case Linux
-            sensible-browser "https://www.google.com/search?q="$argv
-        case Darwin
-            open -na "Google Chrome" --args "https://www.google.com/search?q="$argv
-    end
-end
--- private_dot_config/private_fish/private_functions/private_srm.fish
+++
@@ -1,8 +0,0 @@
-function srm --description 'secure delete'
-    switch (uname)
-        case Linux
-            srm -v $argv
-        case Darwin
-            rm -v -P $argv
-    end
-end
